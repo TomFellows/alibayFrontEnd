1d8e7683b00a76dc9939e6e8a5577736b713b353
Anton <anthonycolpron@gmail.com>
2018-08-08 18:10:03 -0400

    item components align bottom

--- src/Components/ItemComponent.js
+++ src/Components/ItemComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ItemDetails from './ItemDetails.js';
 import { BrowserRouter, Route } from 'react-router-dom'
+import '../CSS/ItemComponent.css'
 
 class ItemComponent extends Component {
 
@@ -30,8 +31,12 @@ class ItemComponent extends Component {
 
   render() {
     return (
+      <div className='itemComponent'>
       <div onClick={this.itemDetails} >
         <img src={this.props.item.itemImage} style={{'width':'400px'}} className="img2"/>
+      </div>
+      <div className='itemName'> {this.props.item.itemName}</div>
+      <div> Price: {this.props.item.itemPrice} </div> 
       </div>)
   }
 }
